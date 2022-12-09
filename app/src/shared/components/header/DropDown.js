65ff5d8dd7ac9a22b2e59c89702d270185bbfe60
ahmadeleiwa <ahmadilawa@gmail.com>
2022-12-09 21:59:16 +0200

    header & icons (icon-material)

--- app/src/shared/components/header/DropDown.js
+++ app/src/shared/components/header/DropDown.js
@@ -1,14 +1,19 @@
+import { Button } from "@mui/material";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
 import './DropDown.css'
 
-const DropDown = props =>{
-    return <div className="drop-down" style={{left:props.offset}}>
+const DropDown = props => {
+    return <div className="drop-down" style={{ left: props.offset }} onMouseLeave={props.onMouseLeave}>
         {props.items && props.items.map((item, index) => <NavLink key={index} to={item.path} className="item" >
-            <img src={item.icon}  alt={item.name}/>
-            <p>{item.name}</p>
-    </NavLink>)}
+ 
+                <Button startIcon={item.icon}  size="large" sx={{width:'100%', height:'100%', color:'black'}} >
+                    {item.name}
+                </Button>
+            </NavLink>
+        )}
+        
     </div>
 }
 
--- app/src/shared/components/header/Header.js
+++ app/src/shared/components/header/Header.js
@@ -1,28 +1,49 @@
-import React, {  useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from 'react-router-dom'
 import DropDown from "./DropDown";
 import './Header.css'
-import icon from '../icons/icon.json'
+import { Button, Fade } from '@mui/material'
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
+
+import { Box } from "@mui/material";
+
+
 const Header = props => {
     const [showDropDown, setShowDropDown] = useState(false)
     const [offset, setOfsset] = useState(100)
     const profile = useRef()
+
     const profileClickHandler = event => {
-        setOfsset(profile.current.offsetLeft-profile.current.offsetWidth*2)
+        setOfsset(profile.current.offsetLeft - profile.current.offsetWidth * 1.9)
         setShowDropDown(!showDropDown)
     }
+    
+    useEffect(() => {
+        window.addEventListener('resize', function () {
+            setOfsset(profile.current.offsetLeft - profile.current.offsetWidth * 1.9)
+        })
+    }, [])
 
-    return <div className="header" >
-        <nav  >
+    return <div className="header"  >
+        <nav >
             {props.items && props.items.map((item, index) => {
                 if (item.profile) {
-                    return <button ref={profile} key={index} onClick={profileClickHandler}><img src={item.icon} alt='account' /> </button>
+                    // if the item is the profile icon will be an normal button
+                    return <Button className='b' ref={profile} key={index} onClick={profileClickHandler}>{item.icon} </Button>
                 }
                 else
-                    return <NavLink key={index} to={item.path}><img src={item.icon} alt="item"/> </NavLink>
+                    return <Button key={index}> <NavLink to={item.path}>{item.icon} </NavLink></Button>
             })}
         </nav>
-        {showDropDown && <DropDown offset={offset} items={[{ name: 'account', icon: icon.account, path: '/' }, { name: 'My notes', icon: icon.note, path: '/' }]} />}
+
+        <Box sx={{ display: 'flex' }}>
+            {/* this part for transition the drop-down menu */}
+            <Fade  direction="up" in={showDropDown}><div    ><DropDown offset={offset} items={[
+                { name: 'account', icon: <AccountCircleIcon />, path: '/' },
+                { name: 'My notes', icon: <StickyNote2Icon />, path: '/' }]} /></div></Fade>
+        </Box>
+
     </div>
 }
 
